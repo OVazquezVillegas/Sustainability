c51e5a754f38dd419f5ff472bd9ff586449abe47
Vazquez Villegas <o.m.vazquezvillegas@uu.nl>
2022-01-26 13:52:02 +0100

    Update

--- Dictionary project.R
+++ Dictionary project.R
@@ -3,7 +3,8 @@
 #Packages
 library(bibliometrix)
 library(textmineR)
-
+library(dplyr)
+library(ggplot2)
 
 #1 Selecting sample
 
@@ -19,13 +20,36 @@ setwd("C:/Users/6674828/OneDrive - Universiteit Utrecht/SDG-UU/Sustainability-re
 write.csv(sample, 'sample.csv')
 
 
+#1.4 Opening sample
+setwd("C:/Users/6674828/OneDrive - Universiteit Utrecht/SDG-UU/Sustainability-repository/Analysis")
+data = read.csv("output - Copy.csv")
+sample = data[round(runif(100, min = 0, max = 1000),0),]
+setwd("C:/Users/6674828/OneDrive - Universiteit Utrecht/SDG-UU/Sustainability-repository/Analysis")
+write.csv(sample, 'output.csv', row.names = F)
+sample = read.csv("output.csv")
+
+
+#2 Searching terms from the Ramirez thesaurus  ('Search function. R)
+#2.1 Converting m into a df
+search = as.data.frame(m)
+setwd("C:/Users/6674828/OneDrive - Universiteit Utrecht/SDG-UU/Sustainability-repository/Analysis")
+write.csv(search, 'search.csv', row.names = F)
+
+
+#Opening Ramirez thesaurus
+setwd("C:/Users/6674828/OneDrive - Universiteit Utrecht/SDG-UU/keywords")
+thesaurus <- read.csv("keywords_ramirez.csv")
+
+# Filtering required SDGs
+sdg_7 = filter(thesaurus, Goal == "7")
+
 #2 Extracting terms for SDG 7
 
 #2.1 Importing file with SDG official terminology (goals, targets and indicators)
 setwd("C:/Users/6674828/OneDrive - Universiteit Utrecht/SDG-UU/Sustainability-repository")
 SDG_terms = read.delim("SDG7_terminology.txt", header=FALSE)
 
-dtm <- CreateDtm(doc_vec = SDG_terms$V1, # character vector of documents
+dtm_1_2 <- CreateDtm(doc_vec = SDG_terms$V1, # character vector of documents
                  ngram_window = c(1, 2), # minimum and maximum n-gram length
                  stopword_vec = stopwords::stopwords("en"), # this is the default value
                  lower = TRUE, # lowercase - this is the default value
@@ -35,15 +59,82 @@ dtm <- CreateDtm(doc_vec = SDG_terms$V1, # character vector of documents
                  cpus = 2) # by default, this will be the max number of cpus available
 
 
-dtm_2 <- CreateDtm(doc_vec = SDG_terms$V1, # character vector of documents
-                ngram_window = 2, # minimum and maximum n-gram length
-                stopword_vec = stopwords::stopwords("en"), # this is the default value
-                lower = TRUE, # lowercase - this is the default value
-                remove_punctuation = TRUE, # punctuation - this is the default
-                remove_numbers = TRUE, # numbers - this is the default
-                verbose = FALSE, # Turn off status bar for this demo
-                cpus = 2) # by default, this will be the max number of cpus available
+dtm_2 <- CreateDtm(doc_vec = SDG_terms$V1, 
+                ngram_window = c(2, 2),
+                stopword_vec = stopwords::stopwords("en"),
+                lower = TRUE, 
+                remove_punctuation = TRUE, 
+                remove_numbers = TRUE, 
+                verbose = FALSE, 
+                cpus = 2) 
+
+dtm_2_3 <- CreateDtm(doc_vec = SDG_terms$V1, 
+                   ngram_window = c(2, 3),
+                   stopword_vec = stopwords::stopwords("en"),
+                   lower = TRUE, 
+                   remove_punctuation = TRUE, 
+                   remove_numbers = TRUE, 
+                   verbose = FALSE, 
+                   cpus = 2) 
 
 
 tf <- TermDocFreq(dtm = dtm)
-vocabulary <- tf$term[ tf$term_freq > 5 ]
+tf_2 <- TermDocFreq(dtm = dtm_2)
+tf_3 <- TermDocFreq(dtm = dtm_3)
+
+
+# Unspervised machine learning model (LDA)
+
+# Importing file with stopwords
+setwd("C:/Users/6674828/OneDrive - Universiteit Utrecht/SDG/TIPC Research Agenda/Data analysis/Data analysis")
+stopwords <- read.csv("stopwords.txt", stringsAsFactors = F)
+stopwords <- stopwords$a
+
+#Creating dtm (TextmineR)
+dtm <- CreateDtm(doc_vec = sample$AB, doc_names = sample$UT,
+                        ngram_window = c(2, 3), 
+                        stopword_vec = c(stopwords, stopwords("en")),
+                        remove_numbers = TRUE,
+                        remove_punctuation = F,
+                        verbose = FALSE, 
+                        cpus = 2)
+
+tf_sample <- TermDocFreq(dtm = dtm)
+
+vocabulary <- tf_sample$term[ tf_sample$term_freq > 1 ]
+
+#Evaluating models (k= 1:20) https://towardsdatascience.com/beginners-guide-to-lda-topic-modelling-with-r-e57a5a8e7a25
+k_list <- seq(1, 20, by = 1)
+model_dir <- paste0("models_", digest::digest(vocabulary, algo = "sha1"))
+if (!dir.exists(model_dir)) dir.create(model_dir)
+
+model_list <- TmParallelApply(X = k_list, FUN = function(k){
+  filename = file.path(model_dir, paste0(k, "_topics.rda"))
+  
+  if (!file.exists(filename)) {
+    set.seed(12345)
+    m <- FitLdaModel(dtm = dtm, k = k, iterations = 50, method = "gibbs")
+    m$k <- k
+    m$coherence <- CalcProbCoherence(phi = m$phi, dtm = dtm, M = 5)
+    save(m, file = filename)
+  } else {
+    load(filename)
+  }
+  
+  m
+}, export=c("dtm", "model_dir")) # export only needed for Windows machines
+
+#model tuning
+#choosing the best model
+coherence_mat <- data.frame(k = sapply(model_list, function(x) nrow(x$phi)), 
+                            coherence = sapply(model_list, function(x) mean(x$coherence)), 
+                            stringsAsFactors = FALSE)
+
+dev.new(width=5, height=4)
+ggplot(coherence_mat, aes(x = k, y = coherence)) +
+  geom_point() +
+  geom_line(group = 1)+
+  ggtitle("Best Topic by Coherence Score") + theme_minimal() +
+  scale_x_continuous(breaks = seq(1,20,1)) + ylab("Coherence")
+
+
---
+++ Search function.R
@@ -0,0 +1,87 @@
+
+
+##############################
+### STEP 5: KEYWORD SEARCH ###
+##############################
+
+# Input values
+file = 'output'
+output = 'data_searched_full'
+thesaurus = 'ramirez_thesaurus'
+cores = 1
+
+###
+setwd("C:/Users/6674828/OneDrive - Universiteit Utrecht/SDG-UU/Sustainability-repository")
+direct=getwd()
+setwd(paste(direct,'Analysis',sep='/'))
+library(textstem);library(tm);library(parallel)
+data=as.matrix(read.csv(paste(file,".csv",sep="")))
+words=as.matrix(read.csv(paste(thesaurus,'.csv',sep='')))
+
+# The search function
+Search=function(j){
+  if(words[j,'Mode']=='stemmed'){
+    if(words[j,'type.1']==1){
+      if(length(grep(paste("\\b", tolower(words[j,'stemmed']), "\\b", sep=""),datas_stemmed))!=0){paste(grep(paste("\\b", tolower(words[j,'stemmed']), "\\b", sep=""),datas_stemmed),j,
+                                                                                                        unlist(lapply(datas_stemmed[grep(paste("\\b", tolower(words[j,'stemmed']), "\\b", sep=""),datas_stemmed)],function(k){
+                                                                                                          length(unlist(gregexpr(paste("\\b", tolower(words[j,'stemmed']), "\\b", sep=""),k)))})),sep='#')}}
+    else{
+      x=table(unlist(lapply(unlist(strsplit(words[j,'stemmed'],'#')),function(h){grep(paste("\\b",tolower(h),"\\b",sep=""),datas_stemmed)})))
+      if(length(x[x==length(unlist(strsplit(words[j,'stemmed'],'#')))])!=0){paste(names(x[x==length(unlist(strsplit(words[j,'stemmed'],'#')))]),j,
+                                                                                  apply(matrix(unlist(lapply(unlist(strsplit(words[j,'stemmed'],'#')),function(h){unlist(lapply(datas_stemmed[as.numeric(names(x[x==length(unlist(strsplit(words[j,'stemmed'],'#')))]))],
+                                                                                                                                                                                function(k){length(unlist(gregexpr(paste("\\b",h,"\\b",sep=""),k)))}))})),byrow=T,ncol=length(unlist(strsplit(words[j,'stemmed'],'#')))),1,min),sep='#')}}}
+  else{
+    if(words[j,'type.1']==1){
+      if(length(grep(paste('\\b',tolower(words[j,'corrected']),sep=""),datas))!=0){paste(grep(paste('\\b',tolower(words[j,'corrected']),sep=""),datas),j,
+                                                                                         unlist(lapply(datas[grep(paste('\\b',tolower(words[j,'corrected']),sep=""),datas)],function(k){
+                                                                                           length(unlist(gregexpr(paste('\\b',tolower(words[j,'corrected']),sep=""),k)))})),sep='#')}}
+    else{
+      x=table(unlist(lapply(unlist(strsplit(words[j,'corrected'],'#')),function(h){grep(paste('\\b',tolower(h),sep=""),datas)})))
+      if(length(x[x==length(unlist(strsplit(words[j,'corrected'],'#')))])!=0){paste(names(x[x==length(unlist(strsplit(words[j,'corrected'],'#')))]),j,
+                                                                                    apply(matrix(unlist(lapply(unlist(strsplit(words[j,'corrected'],'#')),function(h){unlist(lapply(datas[as.numeric(names(x[x==length(unlist(strsplit(words[j,'corrected'],'#')))]))],
+                                                                                                                                                                                    function(k){length(unlist(gregexpr(paste('\\b',h,sep=""),k)))}))})),byrow=T,ncol=length(unlist(strsplit(words[j,'corrected'],'#')))),1,min),sep='#')}}}
+}
+
+# Prepare thesaurus and data
+words[,'corrected']=unlist(lapply(words[,'corrected'], function(x) gsub('”', '\\\\b', x)))
+words[,'corrected']=unlist(lapply(words[,'corrected'], function(x) gsub('\\*', '', x)))
+datas=data[,c('TI','AB','DE')]
+data[,'UT']=substring(data[,'UT'],1,30)
+datas=gsub("  "," ",gsub('[[:digit:]]+', '', gsub('  ',' ',gsub('[[:punct:] ]+',' ',tolower(unlist(mclapply(1:dim(datas)[1],function(i){paste(datas[i,1],datas[i,2],datas[i,3])},mc.cores=cores)))))))
+datas=gsub("  "," ",unlist(mclapply(datas,function(i){paste(unlist(strsplit(i,' '))[!unlist(strsplit(i,' '))%in%stopwords()],collapse=" ")},mc.cores=cores)))
+datas_stemmed=gsub("  "," ",unlist(mclapply(datas,function(i){paste(stem_words(unlist(strsplit(i,' '))[!unlist(strsplit(i,' '))%in%stopwords()]),collapse=" ")},mc.cores=cores)))
+
+# Search each publication for the keywords 
+search=matrix(unlist(strsplit(unlist(mclapply(1:dim(words)[1],Search,mc.cores=cores)),"#")),byrow=T,ncol=3)
+m=matrix(unlist(strsplit(unlist(lapply(1:dim(search)[1],function(i){paste(data[as.numeric(search[i,1]),'UT'], words[as.numeric(search[i,2]),2],words[as.numeric(search[i,2]),1],words[as.numeric(search[i,2]),3],search[i,3],sep='##')})),'##')),byrow=T,ncol=5)
+colnames(m)=c('UT','KeyWord','SDG','Type','Freq')
+write.csv(m,paste(output,"Words_Searched.csv",sep='_'),row.names=F)
+
+# Determine the SDG(s) for each publication
+datas=xtabs(as.numeric(m[,'Freq'])~m[,'UT']+m[,'SDG'])
+datas=cbind(datas,rowSums(datas))
+colnames(datas)=c(paste('SDG', 1:17, sep = '.'),'FreqT');class(datas)='numeric'
+data=data[data[,'UT']%in%rownames(datas),]
+datas=datas[unlist(mclapply(1:dim(data)[1],function(i){which(rownames(datas)==data[i,'UT'])},mc.cores=cores)),]
+data=data.frame(data,datas)
+SDG=unlist(mclapply(1:dim(data)[1],function(i){ifelse(sum(sort(data[i,paste('SDG.',1:17, sep='')]/data[i,'FreqT'],decreasing=T)[1])>.75,
+                                                      paste(names(sort(data[i,paste('SDG.',1:17, sep='')]/data[i,'FreqT'],decreasing=T))[1],collapse='#'),
+                                                      ifelse(sum(sort(data[i,paste('SDG.',1:17, sep='')]/data[i,'FreqT'],decreasing=T)[1:2])>.60,
+                                                             paste(names(sort(data[i,paste('SDG.',1:17, sep='')]/data[i,'FreqT'],decreasing=T))[1:2],collapse='#'),
+                                                             ifelse(sum(sort(data[i,paste('SDG.',1:17, sep='')]/data[i,'FreqT'],decreasing=T)[1:3])>.50,
+                                                                    paste(names(sort(data[i,paste('SDG.',1:17, sep='')]/data[i,'FreqT'],decreasing=T))[1:3],collapse='#'),
+                                                                    paste(names(sort(data[i,paste('SDG.',1:17, sep='')]/data[i,'FreqT'],decreasing=T))[1:3],collapse='#'))))},mc.cores=cores))
+
+data=cbind(data,SDG)
+
+fields = c('UT', 'ACOUSTICS', 'ASTRONOMY & ASTROPHYSICS', 'CRYSTALLOGRAPHY', 'OPTICS')
+delete=NULL
+for (index in grep(paste(fields,collapse="|"),data$SC)){field=data[index,'SC']
+if(length(unlist(strsplit(field,";")))==1){delete=append(delete,data[index,'UT'])}
+else if(length(grep(paste(fields,collapse="|"),unlist(strsplit(field,";"))))==length(unlist(strsplit(field,";")))){
+  delete=append(delete,data[index,'UT'])}}
+data=subset(data,!UT%in%delete)
+
+# Write output 
+write.csv(data,paste(output,'.csv',sep=''),row.names=F)
+setwd(direct)
